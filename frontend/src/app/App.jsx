13ea1fa343df10d2ed46bec792fec3a9f699fc60
Sarthak <sarthakkrishak1234@gmail.com>
2026-07-18 01:06:27 +0530

    Fix Smooth Scroll import path

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { toast } from "react-hot-toast";
 import AppRoutes from "./routes.jsx";
 import { useUser } from "../context/useUserContext.jsx";
-import SmoothScroll from "../components/common/SmoothScroll.jsx";
+// import SmoothScroll from "../components/common/SmoothScroll.jsx";
 
 function App() {
   const navigate = useNavigate();
